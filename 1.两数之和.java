7b85079a23b543b14fe9bcf1e854dc1d3da65723
8790 <2467042336@qq.com>
2020-01-21 04:19:20 +0800

    使用哈希表进行时间优化

--- 1.两数之和.java
+++ 1.两数之和.java
@@ -7,19 +7,19 @@
 // @lc code=start
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[] result = new int[2];
-        for (int i = 0; i < nums.length - 1; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if(nums[i] + nums[j] == target) {
-                    result[0] = i;
-                    result[1] = j;
-                    return result;
-                }
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            int result = target - nums[i];
+            if (map.containsKey(result) && map.get(result) != i) {
+                return new int[] { i, map.get(result) };
             }
         }
 
-        return result;
+        return null;
     }
 }
 // @lc code=end
-
