79ffc18f4cce320ba476ecbf499596ffb443a734
8790 <2467042336@qq.com>
2020-01-21 17:03:42 +0800

    使用哈希表进行一次遍历找出元素

--- 1.两数之和.java
+++ 1.两数之和.java
@@ -1,3 +1,5 @@
+import java.util.Map;
+
 /*
  * @lc app=leetcode.cn id=1 lang=java
  *
@@ -9,14 +11,12 @@ class Solution {
     public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            map.put(nums[i], i);
-        }
-
-        for (int i = 0; i < nums.length; i++) {
             int result = target - nums[i];
-            if (map.containsKey(result) && map.get(result) != i) {
-                return new int[] { i, map.get(result) };
+            if (map.containsKey(result)) {
+                return new int[] { map.get(result), i };
             }
+            map.put(nums[i], i);
+
         }
 
         return null;
