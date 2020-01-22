bd1e4a886edd857edba1fe0f439fee8b051c5e49
8790 <2467042336@qq.com>
2020-01-22 21:00:36 +0800

    进行代码优化，将两个变量缩减成一个变量，去掉大量判断语句。

--- 2.两数相加.java
+++ 2.两数相加.java
@@ -17,34 +17,21 @@ class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode head = new ListNode(0);
         ListNode p = head;
-        int carry = 0;
-        while(l1 != null || l2 != null || carry == 1) {
-            int add = 0;
-            if(l1 == null && l2 != null) {
-                add = l2.val + carry;
-                l2 = l2.next;
-            } else if (l2 == null && l1 != null) {
-                add = l1.val + carry;
+        int carry = 0;  //将进位和累加全部放在这一个变量中。
+        while(l1 != null || l2 != null || carry != 0) {
+            if(l1 != null) {
+                carry += l1.val;
                 l1 = l1.next;
-            } else if (l1 == null && l2 == null) {
-                add = carry;
-            } else {
-                add = l1.val + l2.val + carry;
-                l1 = l1.next;
+            } 
+            if (l2 != null) {
+                carry += l2.val;
                 l2 = l2.next;
-            }
-            // 进位判断
-            if (add > 9) {
-                carry = 1;
-                add = add % 10;
-            } else {
-                carry = 0;
             }
             //新建结点，计算高位
-            p.next = new ListNode(add);
+            p.next = new ListNode(carry % 10);
+            carry /= 10;
             p = p.next;          
         }
-        
         return head.next;
     }
 }
